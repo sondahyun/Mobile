4af1f3433dffbc35bd126a268c14ca8d7e459ad6
sondahyun <88223753+sondahyun@users.noreply.github.com>
2024-10-17 21:30:13 +0900

    week05

--- week05/ArchictectureRef/ArchictectureRef/app/src/main/java/ddwu/com/mobileapp/week05/archictectureref/ui/RefViewModel.kt
+++ week05/ArchictectureRef/ArchictectureRef/app/src/main/java/ddwu/com/mobileapp/week05/archictectureref/ui/RefViewModel.kt
@@ -16,13 +16,19 @@ class RefViewModel (val refRepository: RefRepository) : ViewModel() {
     val allRefs : LiveData<List<RefEntity>> = refRepository.allRefs.asLiveData()
 
     // one-shot 결과를 확인하고자 할 때 사용
+    // LiveData는 값 변경 불가: MutableLiveData는 값 변경 가능
+    // 이용해서 간접적으로 값 바꿈
     private var _name = MutableLiveData<String>()
-    val nameData = _name
+    
+    // ViewModel에서 화면이 파괴되도 사용하려면 멤버변수로 저장
+    // MutableLiveData의 _name을 가르킴
+    // _name이 바뀌면 nameData도 바뀜
+    val nameData : LiveData<String> = _name
 
     // viewModelScope 는 Dispatcher.Main 이므로 긴시간이 걸리는 IO 작업은 Dispatchers.IO 에서 작업
-    fun findName(id: Int) = viewModelScope.launch {
+    fun findName(id: Int) = viewModelScope.launch { // CoroutineScope(Dispatchers.Main).launch
         var result : String
-        withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO) { // Main에서 DB 작업하면 막힘 -> IO로 쓰레드 사용
             result = refRepository.getNameById(id)
         }
         _name.value = result
