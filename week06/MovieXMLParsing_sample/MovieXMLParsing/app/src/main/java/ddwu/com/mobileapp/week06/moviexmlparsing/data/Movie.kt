b43f9a75146561ed21d4f3c56a63f13640dd21a4
sondahyun <sonda2356@naver.com>
2024-10-19 14:58:00 +0900

    week06

--- week06/MovieXMLParsing_sample/MovieXMLParsing/app/src/main/java/ddwu/com/mobileapp/week06/moviexmlparsing/data/Movie.kt
+++ week06/MovieXMLParsing_sample/MovieXMLParsing/app/src/main/java/ddwu/com/mobileapp/week06/moviexmlparsing/data/Movie.kt
@@ -2,4 +2,11 @@ package ddwu.com.mobileapp.week06.naverxmlparsing.data
 
 data class Movie(
     /* Parsing 결과를 저장할 Movie 의 멤버 선언 */
-)
+    var rank: Int?,
+    var title: String?,
+    var openData: String?,
+) {
+    override fun toString(): String {
+        return "$rank - $title ($openData)"
+    }
+}
