822e7f5a33e173eeda0f05a4df55fd90bb13a36a
sondahyun <sonda2356@naver.com>
2024-11-14 02:24:35 +0900

    week10

--- week10/NewNotiTest/app/src/main/java/ddwu/com/mobileapp/week10/newnotitest/AlertBroadcastReceiver.kt
+++ week10/NewNotiTest/app/src/main/java/ddwu/com/mobileapp/week10/newnotitest/AlertBroadcastReceiver.kt
@@ -7,18 +7,18 @@ import android.util.Log
 import android.widget.Toast
 
 class AlertBroadcastReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context?, intent: Intent?) { // intent 들어옴
 
         Toast.makeText(context, "휴식중...", Toast.LENGTH_LONG ).show()
 
         val notiId = intent?.getIntExtra("NOTI_ID", 0)      // intent 에 기록한 값 확인
         Log.d("AlertBroadcastReceiver", "Notification ID: ${notiId}")
 
-        /*알림을 선택하면 MainActivity 실행*/
-        val intent = Intent (context, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        context?.startActivity(intent)
+//        /*알림을 선택하면 MainActivity 실행*/
+//        val intent = Intent (context, MainActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//        context?.startActivity(intent)
 
     }
 }
--- week10/NewNotiTest/app/src/main/java/ddwu/com/mobileapp/week10/newnotitest/MainActivity.kt
+++ week10/NewNotiTest/app/src/main/java/ddwu/com/mobileapp/week10/newnotitest/MainActivity.kt
@@ -37,10 +37,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(mainBinding.root)
 
+        // 앱이 실행되자마자 채널 생성 (T or F)
         createNotificationChannel()
 
         mainBinding.btnNoti.setOnClickListener {
             Thread {
+                // 3초 대기
                 sleep(3000)
                 showNotification()
             }.start()
@@ -55,19 +57,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // 채널 생성 (앱이 실행될때)
     private fun createNotificationChannel() {
+        // 최소 버전 (O = 오레오 버전)보다 크면 channel 만듦
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Notification Channel 의 생성
 
             val name = "Test Channel"
             val descriptionText = "Test Channel Message"
+            // 알림의 우선 순위
             val importance = NotificationManager.IMPORTANCE_DEFAULT
+            // channel 생성
             val mChannel = NotificationChannel(channelID, name, importance)
-            mChannel.description = descriptionText
+            mChannel.description = descriptionText // description 삽입
 
             // Channel 을 시스템에 등록, 등록 후에는 중요도 변경 불가
             val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(mChannel)
+            // 시스템에게 channel이 사용 가능한지 확인 (T or F)
             Toast.makeText(applicationContext, "${notificationManager.areNotificationsEnabled()}", Toast.LENGTH_SHORT).show()
 
 //            notificationManager.deleteNotificationChannel(channelID)    // 채널 삭제
@@ -75,54 +82,77 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // 알림 생성
     private fun showNotification() {
         checkNotificationPermission()
-
-        val intent = Intent (this, AlertActivity::class.java).apply {
+        // activity에서 activity 띄움 (알림 누르면 AlertActivity 띄움)
+        val intent = Intent(this, AlertActivity::class.java).apply {
+            // 이전 알림 삭제하고 새로 띄우기
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
-        val pendingIntent : PendingIntent
-        = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        // intent를 pendingIntent로 포장해서 시스템에 전달
+        // Activity 포장 -> getActivity
+        // BroadCast 포장 -> getBroadcast, System 포장 -> getSystem, requestCode -> pendingIntent 구분 용도
+        // 변경 불가능
+        val pendingIntent: PendingIntent
+                = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        val newNotiBuilder = NotificationCompat.Builder(this, channelID)
-            .setSmallIcon(R.drawable.ic_stat_name)
+        // Builder 객체
+        val newNoti = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_stat_name) // Image Asset
             .setContentTitle("알림 제목")
             .setContentText("짧은 알림 내용")
-            .setStyle(NotificationCompat.BigTextStyle().bigText("확장 시 확인할 수 있는 긴 알림 내용"))
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("확장시 확인할 수 있는 긴 알림 내용"))
+            // 8.0 이상에서는 대신 Channel의 중요도로 설정
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+            .setContentIntent(pendingIntent) // 시스템에 전달할 Intent
+            .setAutoCancel(true) // 클릭하면 바로 닫힘
 
+        // notiManager
         val notiManager = NotificationManagerCompat.from(this)
-        notiManager.notify(100, newNotiBuilder.build())
+        // 알림 실행 (식별 아이디, noti 객체)
+        val noti : Notification = newNoti.build() // 객체 생성
+        notiManager.notify(100, noti)
     }
 
 
     private fun showNotificationWithAction() {
         checkNotificationPermission()
 
-        val intent = Intent (this, AlertBroadcastReceiver::class.java).apply {
-            action = "ACTION_SNOOZE"
-            putExtra("NOTI_ID", 200)
+        // activity에서 br 띄움 (알림 누르면 AlertActivity 띄움)
+        val intent = Intent(this, AlertBroadcastReceiver::class.java).apply {
+            action = "ACTION_SNOOZE" // br의 종류 (방송 정보)
+            putExtra("NOTI_ID", 200) // 전달하는 값 (방송 내용)
         }
 
-        val pendingIntent : PendingIntent
+        // intent를 pendingIntent로 포장해서 시스템에 전달
+        // Activity 포장 -> getActivity
+        // BroadCast 포장 -> getBroadcast, System 포장 -> getSystem, requestCode -> pendingIntent 구분 용도
+        // 변경 불가능
+        val pendingIntent: PendingIntent
                 = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        val newNotiBuilder = NotificationCompat.Builder(this, channelID)
-            .setSmallIcon(R.drawable.ic_stat_name)
+        // Builder 객체
+        val newNoti = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_stat_name) // Image Asset
             .setContentTitle("알림 제목")
             .setContentText("짧은 알림 내용")
-            .setStyle(NotificationCompat.BigTextStyle().bigText("확장 시 확인할 수 있는 긴 알림 내용"))
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("확장시 확인할 수 있는 긴 알림 내용"))
+            // 8.0 이상에서는 대신 Channel의 중요도로 설정
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+            .setContentIntent(pendingIntent) // 시스템에 전달할 Intent
+            .setAutoCancel(true) // 클릭하면 바로 닫힘
+            // 알림 메시지에 버튼을 만듦
             .addAction(R.drawable.ic_stat_name, "쉬기", pendingIntent)
 
-
+        // notiManager
         val notiManager = NotificationManagerCompat.from(this)
-        notiManager.notify(100, newNotiBuilder.build())
+        // 알림 실행 (식별 아이디, noti 객체)
+        val noti : Notification = newNoti.build() // 객체 생성
+        notiManager.notify(100, noti)
     }
 
 
