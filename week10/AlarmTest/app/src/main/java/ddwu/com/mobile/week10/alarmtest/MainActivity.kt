ed4aa334a240aa93e64f8037c00a9434c4a63e20
sondahyun <sonda2356@naver.com>
2024-11-16 21:21:35 +0900

    week10

--- week10/AlarmTest/app/src/main/java/ddwu/com/mobile/week10/alarmtest/MainActivity.kt
+++ week10/AlarmTest/app/src/main/java/ddwu/com/mobile/week10/alarmtest/MainActivity.kt
@@ -33,21 +33,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         mainBinding.btnOneShot.setOnClickListener {
-            checkNotificationPermission()   // 알림 권한 확인, 승인할 경우 onRequestPermissionsResult() 에서 채널 생성
+            val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-            /*알람 실행 시 MyBroadcastRecevier 실행, ALARM_MESSAGE 전달*/
-            val intent = Intent (this, MyBroadcastReceiver::class.java).apply {
-                action = "ddwu.com.mobile.week10.alarmtest.ACTION_ALARM"
-                putExtra("ALARM_MESSAGE", "일어나세요!")
-            }
+            var intent = Intent(this, MyBroadcastReceiver::class.java)
 
-            val pendingIntent : PendingIntent
-                    = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            val requestId = 100 // 삭제할 때 사용
 
-            /*현재 경과시간 10초 뒤에 일회성 알람 실행*/
-            alarmManager?.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime() + 10 * 1000,
-                pendingIntent)
+            val pendingIntent =
+                PendingIntent.getBroadcast(applicationContext, requestId, intent, PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE)
+
+            manager.set(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 20 * 1000, // 10초 뒤에 울림
+                pendingIntent
+            )
         }
 
 
--- week10/AlarmTest/app/src/main/java/ddwu/com/mobile/week10/alarmtest/MyBroadcastReceiver.kt
+++ week10/AlarmTest/app/src/main/java/ddwu/com/mobile/week10/alarmtest/MyBroadcastReceiver.kt
@@ -10,32 +10,7 @@ import androidx.core.app.NotificationManagerCompat
 
 class MyBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
-        /*전달 받은 intent 에서 Message 확인*/
-        val alarmMessage= intent?.getStringExtra("ALARM_MESSAGE")
-        Log.d("MyBroadcastReceiver", "ALARM_MESSAGE: ${alarmMessage}")
-
-        /*알림 클릭 시 MainActivity 실행을 위한 Intent 준비*/
-        val newIntent = Intent (context, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        val pendingIntent : PendingIntent
-                = PendingIntent.getActivity(context, 0, newIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        val channelID = context?.resources?.getString(R.string.channel_id)!!
-
-        /*알림 표시*/
-        val alarmNotiBuilder = NotificationCompat.Builder(context!!, channelID)
-            .setSmallIcon(R.drawable.ic_stat_name)
-            .setContentTitle("알람!!!")
-            .setContentText(alarmMessage)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        val notiManager = NotificationManagerCompat.from(context)
-        notiManager.notify(100, alarmNotiBuilder.build())
-
+        Log.d("MyBr", "Alarm!!!")
     }
 
 }
